1d3e165d2b36f4030e9e47c86b3ac827a2a77df1
MoonSovering <josesmc99@gmail.com>
2022-12-28 11:04:55 -0500

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import { AuthRoute } from "../auth/routes/AuthRoute"
+import { JournalRoutes } from "../journal/routes/JournalRoutes"
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+        //Login y registro
+        <Route path="/auth/*" element={ <AuthRoute /> }/>
+
+        //Journal app
+        <Route path="/*" element={ <JournalRoutes /> }/>
+    </Routes>
+  )
+}
